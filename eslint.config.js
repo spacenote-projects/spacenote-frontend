f8c387797b4dc3ad32173382eef085d0a8ecdd26
mcbarinov <mcbarinov@proton.me>
2025-09-12 18:24:44 +0300

    update

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,60 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from "@eslint/js"
+import globals from "globals"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
+import tseslint from "typescript-eslint"
+import reactX from "eslint-plugin-react-x"
+import reactDom from "eslint-plugin-react-dom"
+import prettier from "eslint-config-prettier"
+import pluginQuery from "@tanstack/eslint-plugin-query"
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+import { defineConfig, globalIgnores } from "eslint/config"
+
+export default defineConfig([
+  globalIgnores(["dist", "public/mockServiceWorker.js"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
+      ...tseslint.configs.strictTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+      ...pluginQuery.configs["flat/recommended"],
+      reactX.configs["recommended-typescript"],
+      reactDom.configs.recommended,
+      reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
+      prettier,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-misused-promises": [
+        "error",
+        {
+          checksVoidReturn: {
+            attributes: false,
+          },
+        },
+      ],
+    },
+  },
+  {
+    files: ["**/components/ui/**/*.{ts,tsx}"],
+    rules: {
+      "@typescript-eslint/consistent-type-definitions": "off",
+      "@typescript-eslint/no-unnecessary-condition": "off",
+      "@typescript-eslint/no-unnecessary-template-expression": "off",
+      "@typescript-eslint/no-unnecessary-type-conversion": "off",
+      "react-refresh/only-export-components": "off",
+      "react-x/no-context-provider": "off",
+      "react-x/no-use-context": "off",
+      "react-x/no-unstable-context-value": "off",
     },
   },
 ])
---
+++ justfile
@@ -0,0 +1,6 @@
+dev:
+    pnpm run dev
+
+lint:
+    pnpm run format
+    pnpm run lint
